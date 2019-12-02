9a174d7ad2769949b80b96362b427be308aff589
Lars <lars.christensen20@gmail.com>
2019-12-02 20:28:04 +0100

    added random advice when clicking image in dashboard, added random achievements when clicking image in achievement tab.

--- app/src/main/java/dk/sdu/mmmi/iss/behealthy/view/AchievementActivity.java
+++ app/src/main/java/dk/sdu/mmmi/iss/behealthy/view/AchievementActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import dk.sdu.mmmi.iss.behealthy.R;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class AchievementActivity extends AppCompatActivity {
 
@@ -11,5 +15,23 @@ public class AchievementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement);
+
+        findViewById(R.id.achievementImageView).setOnClickListener(v -> Toast.makeText(this, randomToast(), Toast.LENGTH_SHORT).show());
+    }
+
+    public String randomToast() {
+        ArrayList<String> adviceArrayList = new ArrayList<>();
+        adviceArrayList.add("Nice! You're only 10 % away from your daily step goal!");
+        adviceArrayList.add("Nice! You're only 5 % away from your daily running goal!");
+        adviceArrayList.add("Its very nice weather today, you can safely exercise outdoors!");
+        adviceArrayList.add("Well done! You have achieved your daily goal!");
+        adviceArrayList.add("Well done! You have run 250 km. in total!");
+        adviceArrayList.add("Well done! You have biked 1000 km. in total!");
+        adviceArrayList.add("Well done! You have achieved a new blood pressure high score!");
+        adviceArrayList.add("Well done! You have achieved a new record in running!");
+
+        Random ran = new Random();
+        int x = ran.nextInt(8) ;
+        return adviceArrayList.get(x);
     }
 }
--- app/src/main/java/dk/sdu/mmmi/iss/behealthy/view/DashboardActivity.java
+++ app/src/main/java/dk/sdu/mmmi/iss/behealthy/view/DashboardActivity.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import dk.sdu.mmmi.iss.behealthy.R;
 import dk.sdu.mmmi.iss.behealthy.model.DataProvider;
 
@@ -33,6 +36,22 @@ public class DashboardActivity extends AppCompatActivity {
         findViewById(R.id.fitnessPlan).setOnClickListener(v -> startActivity(new Intent(this, FitnessPlanActivity.class)));
         findViewById(R.id.nutritionPlan).setOnClickListener(v -> startActivity(new Intent(this, NutritionPlanActivity.class)));
         findViewById(R.id.achievementBtn).setOnClickListener(v -> startActivity(new Intent(this, AchievementActivity.class)));
+        findViewById(R.id.imageView).setOnClickListener(v -> Toast.makeText(this, randomAdvice(), Toast.LENGTH_SHORT).show());
+    }
+
+    public String randomAdvice() {
+        ArrayList<String> adviceArrayList = new ArrayList<>();
+        adviceArrayList.add("You have hit your daily exercise goal!");
+        adviceArrayList.add("You need to hit 2389 kcal today, you only have 843! Keep eating healthy!");
+        adviceArrayList.add("Its very nice weather today, you can safely exercise outdoors!");
+        adviceArrayList.add("You should go for a run!");
+        adviceArrayList.add("If you want to get some extra exercise, try walking for 20 minutes!");
+        adviceArrayList.add("You need to exercise more regularly to hit your goals");
+        adviceArrayList.add("Well done! You only need to bike 2 km. to hit your daily goal");
+
+        Random ran = new Random();
+        int x = ran.nextInt(7) ;
+        return adviceArrayList.get(x);
     }
 
 }
